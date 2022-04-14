91e0936016b2deb4e5a566a5a3a3babe921c2840
Daniel Sanchez <danxschz@gmail.com>
2022-04-14 00:01:10 -0400

    Build display, clear and delete functions

--- calculator.js
+++ calculator.js
@@ -19,6 +19,10 @@ function remainder(a, b) {
   return a % b;
 }
 
+function round(value, precision) {
+  var multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
 
 // Main functions
 function operate(operator, a, b) {
@@ -40,11 +44,13 @@ function evaluate() {
   // Convert from string to number
   firstValue = (firstValue === '') ? currentTotal : +firstValue; // If a is empty it will take the current accumulated value
   secondValue = +secondValue;
-  currentTotal = operate(operator, firstValue, secondValue);
+  if ((secondValue === undefined) || (operator === '')) return;
+  currentTotal = round(operate(operator, firstValue, secondValue), 4);
+  displayTop.textContent = `${firstValue} ${operator} ${secondValue} =`;
+  displayBottom.textContent = currentTotal;
   operator = '';
   firstValue = '';
   secondValue = '';
-  console.log(currentTotal);
 }
 
 
@@ -53,15 +59,25 @@ let firstValue = '';
 let secondValue = '';
 let operator = '';
 let currentTotal = '';
+const displayBottom = document.querySelector('.bottom');
+const displayTop = document.querySelector('.top');
 
 // Number buttons
 const numberButtons = document.querySelectorAll('.number');
 numberButtons.forEach(button => {
   button.addEventListener('click', () => {
     if (operator === '') {
+      if (firstValue === '') {
+        displayBottom.textContent = '';
+        displayTop.textContent = '';
+        currentTotal = '';
+      }
       firstValue += button.textContent;
     } else {
       secondValue += button.textContent;
+    }
+    if (displayBottom.textContent.length < 20) {
+      displayBottom.textContent += button.textContent;
     }
     console.log(`a:${firstValue} operator:${operator} b:${secondValue}`);
   });
@@ -73,9 +89,15 @@ operatorButtons.forEach(button => {
   button.addEventListener('click', () => {
     if (secondValue === '') {
       operator = button.textContent;
+      if (displayBottom.textContent.length < 20) {
+        displayBottom.textContent += ` ${operator} `;
+      }
     } else {
       evaluate()
       operator = button.textContent;
+      if (displayBottom.textContent.length < 20) {
+        displayBottom.textContent += ` ${operator} `;
+      }
     }
   });
 });
@@ -86,3 +108,34 @@ equalButton.addEventListener('click', () => {
   evaluate();
 });
 
+// Clear button
+function clear() {
+  displayTop.textContent = '';
+  displayBottom.textContent = '';
+  operator = '';
+  firstValue = '';
+  secondValue = '';
+  currentTotal = '';
+}
+const clearButton = document.querySelector('.clear');
+clearButton.addEventListener('click', () => {
+  clear();
+})
+
+// Delete button
+const deleteButton = document.querySelector('.delete');
+deleteButton.addEventListener('click', () => {
+  if ((!(currentTotal === '')) && (operator === '')) {
+    clear();
+  } else if (operator === '') {
+    firstValue = firstValue.slice(0,-1);
+    displayBottom.textContent = displayBottom.textContent.slice(0,-1);
+    console.log(firstValue);
+  } else if (secondValue === '') {
+    operator = '';
+    displayBottom.textContent = displayBottom.textContent.slice(0,-3);
+  } else {
+    secondValue = secondValue.slice(0,-1);
+    displayBottom.textContent = displayBottom.textContent.slice(0,-1);
+  }
+});
