efdf013a91094f0bef952089a5ff81daf50800ff
Daniel Sanchez <danxschz@gmail.com>
2022-04-14 16:30:26 -0400

    Refactor code and implement keyboard support
    Restructure code into functions, allowing a cleaner addition of
    keyboard support.

--- calculator.js
+++ calculator.js
@@ -2,29 +2,49 @@
 function add(a, b) {
   return a + b;
 }
-
 function subtract(a, b) {
   return a - b;
 }
-
 function multiply(a, b) {
   return a * b;
 }
-
 function divide(a, b) {
   return a / b;
 }
-
 function remainder(a, b) {
   return a % b;
 }
-
 function round(value, precision) {
   var multiplier = Math.pow(10, precision || 0);
   return Math.round(value * multiplier) / multiplier;
 }
 
+
 // Main functions
+function appendNumber(number) {
+  if (operator === '') {
+    if (firstValue === '') {
+      displayBottom.textContent = '';
+      displayTop.textContent = '';
+      currentTotal = '';
+    }
+    firstValue += number;
+  } else secondValue += number;
+
+  if (displayBottom.textContent.length < 20) {
+    displayBottom.textContent += number;
+  }
+}
+
+function appendOperator(sign) {
+  if ((!(operator === '')) && (secondValue === '')) return;
+  if (!(secondValue === '')) evaluate();
+  operator = sign;
+  if (displayBottom.textContent.length < 18) {
+    displayBottom.textContent += ` ${operator} `;
+  }
+}
+
 function operate(operator, a, b) {
   switch(operator) {
     case '+':
@@ -41,16 +61,42 @@ function operate(operator, a, b) {
 }
 
 function evaluate() {
-  // Convert from string to number
-  firstValue = (firstValue === '') ? currentTotal : +firstValue; // If a is empty it will take the current accumulated value
+  // Convert from string to number or append current total if empty
+  firstValue = (firstValue === '') ? currentTotal : +firstValue; 
   secondValue = +secondValue;
+  // Won't evaluate if values are missing
   if ((secondValue === undefined) || (operator === '')) return;
-  currentTotal = round(operate(operator, firstValue, secondValue), 4);
+
+  currentTotal = round(operate(operator, firstValue, secondValue), 4); // Round to max 4 decimals
   displayTop.textContent = `${firstValue} ${operator} ${secondValue} =`;
   displayBottom.textContent = currentTotal;
   operator = '';
   firstValue = '';
   secondValue = '';
+}
+
+function clear() {
+  displayTop.textContent = '';
+  displayBottom.textContent = '';
+  operator = '';
+  firstValue = '';
+  secondValue = '';
+  currentTotal = '';
+}
+
+function deleteCharacter() {
+  if ((!(currentTotal === '')) && (operator === '')) {
+    clear();
+  } else if (operator === '') {
+    firstValue = firstValue.slice(0,-1);
+    displayBottom.textContent = displayBottom.textContent.slice(0,-1);
+  } else if (secondValue === '') {
+    operator = '';
+    displayBottom.textContent = displayBottom.textContent.slice(0,-3);
+  } else {
+    secondValue = secondValue.slice(0,-1);
+    displayBottom.textContent = displayBottom.textContent.slice(0,-1);
+  }
 }
 
 
@@ -65,77 +111,43 @@ const displayTop = document.querySelector('.top');
 // Number buttons
 const numberButtons = document.querySelectorAll('.number');
 numberButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    if (operator === '') {
-      if (firstValue === '') {
-        displayBottom.textContent = '';
-        displayTop.textContent = '';
-        currentTotal = '';
-      }
-      firstValue += button.textContent;
-    } else {
-      secondValue += button.textContent;
-    }
-    if (displayBottom.textContent.length < 20) {
-      displayBottom.textContent += button.textContent;
-    }
-    console.log(`a:${firstValue} operator:${operator} b:${secondValue}`);
-  });
+  button.addEventListener('click', () => appendNumber(button.textContent));
 });
 
 // Operator buttons
 const operatorButtons = document.querySelectorAll('.operator');
 operatorButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    if (secondValue === '') {
-      operator = button.textContent;
-      if (displayBottom.textContent.length < 20) {
-        displayBottom.textContent += ` ${operator} `;
-      }
-    } else {
-      evaluate()
-      operator = button.textContent;
-      if (displayBottom.textContent.length < 20) {
-        displayBottom.textContent += ` ${operator} `;
-      }
-    }
-  });
+  button.addEventListener('click', () => appendOperator(button.textContent));
 });
 
 // Equal button
 const equalButton = document.querySelector('.equal');
-equalButton.addEventListener('click', () => {
-  evaluate();
-});
+equalButton.addEventListener('click', () => evaluate());
 
 // Clear button
-function clear() {
-  displayTop.textContent = '';
-  displayBottom.textContent = '';
-  operator = '';
-  firstValue = '';
-  secondValue = '';
-  currentTotal = '';
-}
 const clearButton = document.querySelector('.clear');
-clearButton.addEventListener('click', () => {
-  clear();
-})
+clearButton.addEventListener('click', () => clear());
 
 // Delete button
 const deleteButton = document.querySelector('.delete');
-deleteButton.addEventListener('click', () => {
-  if ((!(currentTotal === '')) && (operator === '')) {
-    clear();
-  } else if (operator === '') {
-    firstValue = firstValue.slice(0,-1);
-    displayBottom.textContent = displayBottom.textContent.slice(0,-1);
-    console.log(firstValue);
-  } else if (secondValue === '') {
-    operator = '';
-    displayBottom.textContent = displayBottom.textContent.slice(0,-3);
-  } else {
-    secondValue = secondValue.slice(0,-1);
-    displayBottom.textContent = displayBottom.textContent.slice(0,-1);
+deleteButton.addEventListener('click', () => deleteCharacter());
+
+// Keyboard input
+function keyboardInput(e) {
+  if ((e.key >= 0 && e.key <= 9) || (e.key === '.')) appendNumber(e.key);
+  if (e.key === '+' || e.key === '%') appendOperator(e.key);
+  switch(e.key) {
+    case '-':
+      appendOperator('−');
+    case '/':
+      appendOperator('÷');
+    case 'x':
+    case '*':
+      appendOperator('x');
   }
-});
+  if (e.key === 'Enter' || e.key === '=') evaluate();
+  if (e.key === 'Escape') clear();
+  if (e.key === 'Backspace') deleteCharacter();
+}
+
+window.addEventListener('keydown', keyboardInput);
